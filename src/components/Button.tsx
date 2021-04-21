e5261f88f17807b68d3d124af9f8ddbf6630f4cb
Felipe <felipescherer25@gmail.com>
2021-04-21 12:10:29 -0300

    Add external fonts, icons, ux pratices, userinputname and confirmation screen

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-interface ButtonProps extends TouchableOpacityProps{
-    title: string;
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string
 }
 
 export function Button({ title, ...rest } : ButtonProps){
     return (
-        <TouchableOpacity 
-            style={styles.button} 
-            activeOpacity={0.7} 
-            {...rest}>
-            <Text style={styles.buttonText}>
+        //Aways put {...rest} in last
+        <TouchableOpacity style={styles.container} {...rest}> 
+            <Text style={styles.text}>
                 { title }
             </Text>
         </TouchableOpacity>
@@ -20,18 +20,16 @@ export function Button({ title, ...rest } : ButtonProps){
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor:colors.green,
+    container: {
+        backgroundColor: colors.green,
+        height: 56,
+        borderRadius: 16,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        width: 56,
-        paddingHorizontal: 10
     },
-    buttonText: {
+    text: {
+        fontSize: 16,
         color: colors.white,
-        fontSize: 24
+        fontFamily: fonts.heading
     }
 });
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,35 +1,43 @@
-import React, {useState} from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, SafeAreaView, Dimensions, View } from 'react-native';
 
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts'
+
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core'
 
 export function Welcome(){
 
-    const [visible, setVisible] = useState(false);
+    const navigation = useNavigation();
 
-    function handleVisible(){
-        setVisible(!visible)
+    function handleStart(){
+        navigation.navigate('UserIdentification')
     }
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-            Gerencie {'\n'} suas plantas {'\n'} de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                Gerencie {'\n'} suas plantas de {'\n'} forma fácil
+                </Text> 
+                    
+                <Image source={wateringImg} style={styles.image} resizeMode='contain'/>
 
-            {   
-                visible &&
-                <Image source={wateringImg} style={styles.image}/>
-            }
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <Button title='>' onPress={handleVisible}/>
-
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7} 
+                    onPress={handleStart}
+                >          
+                    <Feather name="chevron-right" style={styles.buttonIcon}/>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     );
 }
@@ -37,21 +45,28 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20,
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
         marginTop: 45,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     button: {
         backgroundColor:colors.green,
@@ -62,12 +77,11 @@ const styles = StyleSheet.create({
         height: 56,
         width: 56
     },
-    image: {
-        width: 292,
-        height: 284
-    },
-    buttonText: {
+    buttonIcon: {
+        fontSize: 28,
         color: colors.white,
-        fontSize: 24
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7
     }
 });
